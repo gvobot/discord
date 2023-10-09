97f86c43a872d8b59dbe688a38c3703cc3bee6af
Ducko <duckodas@gmail.com>
2023-10-09 17:25:25 +0200

    Update help.ts
    Fixed the old `help` command

--- packages/bot/src/commands/utility/help.ts
+++ packages/bot/src/commands/utility/help.ts
@@ -1,6 +1,14 @@
 import { DiscordClient } from '../../main.js';
 import { CommandInterface } from '../../types/interfaces.js';
-import { ChatInputCommandInteraction, EmbedBuilder, PermissionFlagsBits, SlashCommandBuilder } from 'discord.js';
+import {
+    ActionRowBuilder,
+    ButtonBuilder,
+    ButtonStyle,
+    ChatInputCommandInteraction,
+    //EmbedBuilder,
+    PermissionFlagsBits,
+    SlashCommandBuilder,
+} from 'discord.js';
 
 const command: CommandInterface = {
     data: new SlashCommandBuilder()
@@ -10,8 +18,24 @@ const command: CommandInterface = {
         .setDefaultMemberPermissions(PermissionFlagsBits.SendMessages)
         .setDMPermission(false),
     execute: async (interaction: ChatInputCommandInteraction, client: DiscordClient) => {
-        let embed = new EmbedBuilder().setColor(client.config.colors.theme).setDescription([``].join('\n'));
-        return interaction.reply({ embeds: [embed], ephemeral: true });
+        return interaction.reply({
+            //embeds: [new EmbedBuilder().setColor(client.config.colors.theme).setDescription(`In development!`)],
+            components: [
+                new ActionRowBuilder<ButtonBuilder>().addComponents(
+                    new ButtonBuilder()
+                        .setCustomId('status')
+                        .setDisabled(true)
+                        .setLabel('Status: Development')
+                        .setStyle(ButtonStyle.Danger),
+                    new ButtonBuilder()
+                        .setDisabled(false)
+                        .setLabel('Support Server')
+                        .setStyle(ButtonStyle.Link)
+                        .setURL('https://discord.gg/h2Pq2Fzw4k'),
+                ),
+            ],
+            ephemeral: true,
+        });
     },
 };
 export default command;
