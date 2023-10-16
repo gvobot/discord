e6051fec0fd5c5aa3a4dff6050ab499e483b33ec
Ducko <duckodas@gmail.com>
2023-10-17 01:52:37 +0200

    Update client.ts
    Updated our daily avatar & presence updater

--- packages/bot/src/events/ready/client.ts
+++ packages/bot/src/events/ready/client.ts
@@ -1,107 +1,68 @@
-import { DiscordClient } from '../../main.js';
-import { EventInterface } from '../../types/interfaces.js';
+import { DiscordClient } from '../../bot.js';
+import { EventInterface } from '../../components/typings/index.js';
 import { Events, ActivityType, PresenceStatusData } from 'discord.js';
-import { logger } from '../../components/logger/index.js';
-import cron from 'node-cron';
+import { logger } from '../../components/handlers/exports.js';
+import moment from 'moment-timezone';
 
 const event: EventInterface = {
     name: Events.ClientReady,
     options: { once: true, rest: false },
     execute: async (client: DiscordClient) => {
-        const today = new Date();
-        const month = today.getMonth() + 1;
-        const day = today.getDate();
-
         const config = client.config;
-
         const avatars = new Map([
             ['halloween', config.avatars.jack_o_lantern],
             ['christmas', config.avatars.snowman],
             ['new_year', config.avatars.star2],
-            ['easter', config.avatars.rabbit],
             ['valentines', config.avatars.gift_heart],
         ]);
-
-        let clientAvatar: string | undefined;
-        let guildAvatar: string | undefined;
-        let presenceActivity: string;
+        let clientAvatar: string | undefined = config.avatars.default;
+        let guildAvatar: string | undefined = config.avatars.default;
+        let presenceActivity: string = 'Saving Humanity! | /help';
         let presenceStatus: PresenceStatusData;
 
-        switch (true) {
-            case month === 11 && day === 13:
-                presenceActivity = 'Happy Birthday Ducko! 🎉🦆';
-                presenceStatus = 'idle';
-                break;
-            case month === 6 && day === 7:
+        const updateActivityAndAvatar = async () => {
+            const now = moment.tz('Europe/Copenhagen');
+            const currentMonth = now.month() + 1;
+            const currentDay = now.date();
+
+            if (currentMonth === 6 && currentDay === 7) {
                 presenceActivity = 'Happy Birthday GVO! 🎉🎂';
                 presenceStatus = 'idle';
-                break;
-            case month === 10 && day == 31:
+            } else if (currentMonth === 10 && currentDay === 31) {
                 clientAvatar = guildAvatar = avatars.get('halloween');
-                presenceActivity = 'Happy Halloween! 🎃👻';
+                presenceActivity = 'Spooky Halloween 🎃👻';
                 presenceStatus = 'idle';
-                break;
-            case month === 12 && day >= 25 && day <= 31:
+            } else if (currentMonth === 12 && currentDay >= 25 && currentDay <= 31) {
                 clientAvatar = guildAvatar = avatars.get('christmas');
-                presenceActivity = 'Merry Christmas! 🎄🎅';
-                presenceStatus = 'dnd';
-                break;
-            case month === 1 && day === 1:
+                presenceActivity = 'Merry Christmas 🎄❄️';
+                presenceStatus = 'idle';
+            } else if (currentMonth === 1 && currentDay === 1) {
                 clientAvatar = guildAvatar = avatars.get('new_year');
-                presenceActivity = 'Happy New Year! 🎉🥳';
-                presenceStatus = 'dnd';
-                break;
-            case month === 4 && day >= 1 && day <= 30:
-                clientAvatar = guildAvatar = avatars.get('easter');
-                presenceActivity = 'Happy Easter! 🐰🥚';
-                presenceStatus = 'online';
-                break;
-            case month === 2 && day === 14:
+                presenceActivity = 'Happy New Year ✨';
+                presenceStatus = 'idle';
+            } else if (currentMonth === 2 && currentDay === 14) {
                 clientAvatar = guildAvatar = avatars.get('valentines');
-                presenceActivity = "Happy Valentine's Day! 💖❤️";
+                presenceActivity = 'Happy Valentines Day 🌺💝💖';
                 presenceStatus = 'dnd';
-                break;
-
-            default:
-                const randomAvatars = [
-                    config.avatars.blush,
-                    config.avatars.grinning,
-                    config.avatars.heart_eyes,
-                    config.avatars.hugging,
-                    config.avatars.joy,
-                    config.avatars.laughing,
-                    config.avatars.stuck_out_tongue,
-                    config.avatars.sunglasses,
-                    config.avatars.thinking,
-                    config.avatars.wink,
-                ];
-                const randomActivities = ['Good Vibes Only!', 'Positive Vibes Only!', 'Chill Vibes Only!'];
-                const randomAvatarIndex = Math.floor(Math.random() * randomAvatars.length);
-                const randomActivityIndex = Math.floor(Math.random() * randomActivities.length);
-                clientAvatar = guildAvatar = randomAvatars[randomAvatarIndex];
-                presenceActivity = randomActivities[randomActivityIndex];
-                presenceStatus = 'online';
-                break;
-        }
-
-        const updateAvatar = async () => {
-            if (clientAvatar && guildAvatar) {
-                await client.user?.setAvatar(clientAvatar);
-                await client.guilds.cache.get(config.guilds[0].id)?.setIcon(guildAvatar);
             }
+
+            try {
+                await client.user?.setAvatar(clientAvatar!);
+                await client.guilds.cache.get(client.config.guilds[0].id)?.setIcon(guildAvatar!);
+                client.user?.setPresence({
+                    activities: [{ type: ActivityType.Custom, name: presenceActivity }],
+                    status: presenceStatus,
+                });
+            } catch (error) {}
         };
 
-        const updatePresence = async () => {
-            client.user?.setPresence({
-                activities: [{ type: ActivityType.Custom, name: presenceActivity }],
-                status: presenceStatus,
-            });
-        };
-
-        cron.schedule('0 0 * * *', updateAvatar);
-        cron.schedule('*/2 * * * *', updatePresence);
-
-        logger.info(`Logged in as ${client.user?.username}`);
+        const now = moment.tz('Europe/Copenhagen');
+        const next12AM = now.add(1, 'days').startOf('day').add(12, 'hours');
+        const delay = next12AM.diff(now);
+        setTimeout(() => {
+            updateActivityAndAvatar(); // Initial change
+            setInterval(updateActivityAndAvatar, 24 * 60 * 60 * 1000);
+        }, delay);
     },
 };
 
