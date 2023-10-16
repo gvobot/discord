83564ec01e49029fe1fab3dffad2153a140d86a2
Ducko <duckodas@gmail.com>
2023-10-17 01:48:47 +0200

    Update discord.ts
    Added three different functions

--- packages/bot/src/components/helpers/discord.ts
+++ packages/bot/src/components/helpers/discord.ts
@@ -1,3 +1,32 @@
-import { logger } from '../logger/index.js';
-import {  } from 'discord.js';
+import { DiscordClient } from '../../bot.js';
+import {} from 'discord.js';
 
+export async function getUsersCount(client: DiscordClient) {
+    try {
+        const results = await client.cluster.broadcastEval('this.users.cache.size');
+        return results.reduce((prev, val) => prev + val, 0);
+    } catch (err) {
+        console.error(err);
+        return 0;
+    }
+}
+
+export async function getChannelsCount(client: DiscordClient) {
+    try {
+        const results = await client.cluster.broadcastEval('this.channels.cache.size');
+        return results.reduce((prev, val) => prev + val, 0);
+    } catch (err) {
+        console.error(err);
+        return 0;
+    }
+}
+
+export async function getGuildsCount(client: DiscordClient) {
+    try {
+        const results = await client.cluster.broadcastEval('this.guilds.cache.size');
+        return results.reduce((prev, val) => prev + val, 0);
+    } catch (err) {
+        console.error(err);
+        return 0;
+    }
+}
