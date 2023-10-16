1da30d93dcfec981082e1566291d5e614dd636fe
Ducko <duckodas@gmail.com>
2023-10-17 01:53:35 +0200

    Config & Interfaces
    Updated config to be more managable
    Updated interfaces of the bot.

--- packages/bot/src/config.ts
+++ packages/bot/src/config.ts
@@ -1,5 +1,5 @@
-import { ConfigInterface } from 'types/interfaces.js';
 import { ColorResolvable } from 'discord.js';
+import { ConfigInterface } from './components/typings/index.js';
 import dotenv from 'dotenv';
 dotenv.config({ path: '../../.env' });
 
@@ -10,25 +10,33 @@ export const config: ConfigInterface = {
         secret: process.env.DISCORD_BOT_SECRET as string,
     },
     avatars: {
-        blush: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_blush.png?raw=true',
-        joy: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_joy.png?raw=true',
-        heart_eyes: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_heart_eyes.png?raw=true',
-        wink: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_wink.png?raw=true',
-        sleepy: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_sleepy.png?raw=true',
-        thinking: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_thinking.png?raw=true',
-        hugging: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_hugging.png?raw=true',
-        grinning: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_grinning.png?raw=true',
-        laughing: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_laughing.png?raw=true',
-        sunglasses: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_sunglasses.png?raw=true',
-        stuck_out_tongue: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_stuck_out_tongue.png?raw=true',
+        default: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_blush.png?raw=true',
 
-        // Events
+        // Event Avatars
         snowman: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_snowman.png?raw=true',
         rabbit: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_rabbit.png?raw=true',
         jack_o_lantern: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_jack_o_lantern.png?raw=true',
         star2: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_star2.png?raw=true',
         gift_heart: 'https://github.com/gvobot/branding/blob/main/avatars/crystal_ball_gift_heart.png?raw=true',
     },
+    webhooks: [
+        {
+            name: 'Guild Blacklisted',
+            url: process.env.GUILD_BLACKLISTED,
+        },
+        {
+            name: 'Guild Unblacklisted',
+            url: process.env.GUILD_UNBLACKLISTED,
+        },
+        // {
+        //     name: 'Guild Added Premium',
+        //     url: process.env.GUILD_ADDED_PREMIUM,
+        // },
+        // {
+        //     name: 'Guild Removed Premium',
+        //     url: process.env.GUILD_REMOVED_PREMIUM,
+        // },
+    ],
     guilds: [
         { name: 'Good Vibes Only™ Support', category: 'support', id: '1126979648249659422' },
         { name: 'Good Vibes Only™ Emojis', category: 'emoji', id: '1159511150464082010' },
